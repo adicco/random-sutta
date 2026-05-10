af8edce0ca61e506d43d497ff85417986eee36c4
Vijjo <vjjdaya@gmail.com>
2026-05-10 19:09:03 +0545

    feat(epub): improve English-only flow by using inline segments

--- src/epub_builder/core/html_builder.py
+++ src/epub_builder/core/html_builder.py
@@ -8,8 +8,16 @@ from .link_resolver import resolve_internal_links
 
 logger = logging.getLogger("EpubBuilder.HtmlBuilder")
 
+
 class HtmlBuilder:
-    def __init__(self, db, all_meta, uid_to_filename, use_apk_links: bool = False, eng_only: bool = False):
+    def __init__(
+        self,
+        db,
+        all_meta,
+        uid_to_filename,
+        use_apk_links: bool = False,
+        eng_only: bool = False,
+    ):
         self.db = db
         self.all_meta = all_meta
         self.uid_to_filename = uid_to_filename
@@ -21,7 +29,7 @@ class HtmlBuilder:
         original = meta.get("original_title")
         acronym = meta.get("acronym")
         m_type = meta.get("type")
-        
+
         # For subleafs, usually acronym is enough and avoids "AN 1.1 - AN 1.1" redundancy
         if m_type == "subleaf" and acronym:
             return acronym
@@ -31,22 +39,35 @@ class HtmlBuilder:
             base_title = f"{translated} - {original}"
         else:
             base_title = translated or original or uid.upper()
-            
-        omit_prefix_uids = {"tpk", "sutta", "vinaya", "abhidhamma", "pli-tv-vi", "pli-tv-ab"}
-        
+
+        omit_prefix_uids = {
+            "tpk",
+            "sutta",
+            "vinaya",
+            "abhidhamma",
+            "pli-tv-vi",
+            "pli-tv-ab",
+        }
+
         if acronym and uid not in omit_prefix_uids:
             return f"{acronym} - {base_title}"
-            
+
         child_range = meta.get("child_range")
-        if child_range and meta.get("type") in ["branch", "root", "group"] and uid not in omit_prefix_uids:
+        if (
+            child_range
+            and meta.get("type") in ["branch", "root", "group"]
+            and uid not in omit_prefix_uids
+        ):
             return f"{child_range} - {base_title}"
-            
+
         return base_title
 
     def build_segment_html(self, segment: Dict[str, Any], footnote_idx: int = 0) -> str:
         segment_id = segment.get("segment_id", "")
-        content_items = segment.get("content_items", []) # We should probably pass the aggregated list here
-        
+        content_items = segment.get(
+            "content_items", []
+        )  # We should probably pass the aggregated list here
+
         # If segment is already aggregated (horizontal format)
         pli = segment.get("pli") or ""
         eng = segment.get("eng") or ""
@@ -68,21 +89,30 @@ class HtmlBuilder:
             if not eng and footnote_idx > 0:
                 pli_text += f' <sup class="footnote-ref"><a class="footnote-link" epub:type="noteref" href="#fn_{segment_id}" id="ref_{segment_id}">{footnote_idx}</a></sup>'
             content += f'<p class="pli">{pli_text}</p>'
-            
+
         if eng:
             eng_text = eng
             if footnote_idx > 0:
                 eng_text += f' <sup class="footnote-ref"><a class="footnote-link" epub:type="noteref" href="#fn_{segment_id}" id="ref_{segment_id}">{footnote_idx}</a></sup>'
-            content += f'<p class="eng">{eng_text}</p>'
             
-        inner_html = f'<div class="segment" id="{segment_id}">\n{content}\n</div>'
-        
+            if self.eng_only:
+                content += f'<span class="eng">{eng_text}</span>'
+            else:
+                content += f'<p class="eng">{eng_text}</p>'
+
+        if self.eng_only:
+            inner_html = f'<span class="segment" id="{segment_id}">{content}</span>'
+        else:
+            inner_html = f'<div class="segment" id="{segment_id}">\n{content}\n</div>'
+
         if html_tag and "{}" in html_tag:
             return html_tag.format(inner_html)
-                
+
         return inner_html
 
-    def generate_page(self, uid: str, pages_list: List[Dict[str, Any]]) -> Optional[Tuple[str, List[Dict[str, str]]]]:
+    def generate_page(
+        self, uid: str, pages_list: List[Dict[str, Any]]
+    ) -> Optional[Tuple[str, List[Dict[str, str]]]]:
         meta = self.all_meta.get(uid)
         if not meta:
             logger.warning(f"Missing metadata for {uid}")
@@ -91,15 +121,16 @@ class HtmlBuilder:
         m_type = meta.get("type", "branch")
         if m_type == "subleaf":
             return None
-            
+
         title = self.get_title(uid, meta)
         safe_uid = uid.replace("/", "_").replace(":", "_")
         filename = f"{safe_uid}.html"
         self.uid_to_filename[uid] = filename
-        
+
         collected_headers = []
-        
+
         import html
+
         if m_type == "leaf":
             # Detect if this leaf has subleaf children to avoid duplicate TOC entries
             has_subleaf_children = False
@@ -127,27 +158,51 @@ class HtmlBuilder:
                 comm = seg.get("comm")
                 footnote_idx = 0
                 if comm:
-                    comm = resolve_internal_links(comm, self.uid_to_filename, self.all_meta)
+                    comm = resolve_internal_links(
+                        comm, self.uid_to_filename, self.all_meta
+                    )
                     current_footnotes.append((seg.get("segment_id", ""), comm))
                     footnote_idx = len(current_footnotes)
 
                 # Only extract headers if the leaf doesn't have virtual subleaf children
-                if not has_subleaf_children and html_tag and any(tag in html_tag for tag in ["<h1", "<h2", "<h3", "<h4", "<h5", "<h6"]):
-                    if any(cls in html_tag for cls in ["class='sutta-title'", 'class="sutta-title"', "class='range-title'", 'class="range-title"']):
-                        pass # Don't add titles to TOC if they are already represented by the page/subleaf entry
+                if (
+                    not has_subleaf_children
+                    and html_tag
+                    and any(
+                        tag in html_tag
+                        for tag in ["<h1", "<h2", "<h3", "<h4", "<h5", "<h6"]
+                    )
+                ):
+                    if any(
+                        cls in html_tag
+                        for cls in [
+                            "class='sutta-title'",
+                            'class="sutta-title"',
+                            "class='range-title'",
+                            'class="range-title"',
+                        ]
+                    ):
+                        pass  # Don't add titles to TOC if they are already represented by the page/subleaf entry
                     else:
                         header_text = seg.get("pli") or seg.get("eng") or "Section"
                         level = 1
-                        if "<h2" in html_tag: level = 2
-                        elif "<h3" in html_tag: level = 3
-                        elif "<h4" in html_tag: level = 4
-                        elif "<h5" in html_tag: level = 5
-                        elif "<h6" in html_tag: level = 6
-                        collected_headers.append({
-                            "title": header_text,
-                            "anchor": seg.get("segment_id", ""),
-                            "level": level
-                        })
+                        if "<h2" in html_tag:
+                            level = 2
+                        elif "<h3" in html_tag:
+                            level = 3
+                        elif "<h4" in html_tag:
+                            level = 4
+                        elif "<h5" in html_tag:
+                            level = 5
+                        elif "<h6" in html_tag:
+                            level = 6
+                        collected_headers.append(
+                            {
+                                "title": header_text,
+                                "anchor": seg.get("segment_id", ""),
+                                "level": level,
+                            }
+                        )
 
                 html_parts.append(self.build_segment_html(seg, footnote_idx))
 
@@ -159,7 +214,7 @@ class HtmlBuilder:
                     fn_html += '  <div class="footnote-container">\n'
                     for i, part in enumerate(parts):
                         if i == 0:
-                            fn_html += f'    <p class="footnote-content first-paragraph"><a class="footnote-backlink" epub:type="backlink" href="#ref_{seg_id}">{idx}</a>{part}</p>\n'
+                            fn_html += f'    <p class="footnote-content first-paragraph"><a class="footnote-backlink" epub:type="backlink" href="#ref_{seg_id}">{idx}.</a>&nbsp;&nbsp;{part}</p>\n'
                         else:
                             fn_html += f'    <p class="footnote-content">{part}</p>\n'
                     fn_html += "  </div>\n</aside>\n"
@@ -172,33 +227,57 @@ class HtmlBuilder:
             if acronym:
                 acronym_div = f'<div class="low-profile-acronym">{acronym}</div>'
                 # Robust regex to catch multi-segment <ul> blocks inside <header>
-                content_html, count = re.subn(r'(<header>.*?)<ul.*?>.*?</ul>', f'\\1{acronym_div}', content_html, flags=re.DOTALL)
+                content_html, count = re.subn(
+                    r"(<header>.*?)<ul.*?>.*?</ul>",
+                    f"\\1{acronym_div}",
+                    content_html,
+                    flags=re.DOTALL,
+                )
                 if count == 0:
                     # If no UL found, prepend acronym to header content
-                    content_html = re.sub(r'(<header>)', f'\\1{acronym_div}', content_html, flags=re.DOTALL)
+                    content_html = re.sub(
+                        r"(<header>)",
+                        f"\\1{acronym_div}",
+                        content_html,
+                        flags=re.DOTALL,
+                    )
             else:
                 # Still remove UL even if no acronym
-                content_html = re.sub(r'(<header>.*?)<ul.*?>.*?</ul>', r'\1', content_html, flags=re.DOTALL)
+                content_html = re.sub(
+                    r"(<header>.*?)<ul.*?>.*?</ul>",
+                    r"\1",
+                    content_html,
+                    flags=re.DOTALL,
+                )
 
             if not content_html.strip():
                 content_html = "<p><i>[No content available]</i></p>"
 
-            page_html = PAGE_HTML_TEMPLATE.format(title=html.escape(title), content=content_html)
+            page_html = PAGE_HTML_TEMPLATE.format(
+                title=html.escape(title), content=content_html
+            )
             pages_list.append({"filename": filename, "content": page_html})
 
         elif m_type in ["branch", "root", "group"]:
             blurb = meta.get("blurb") or ""
             page_html = BRANCH_HTML_TEMPLATE.format(
-                title=html.escape(title),
-                blurb=blurb,
-                children_links="{children_links}"
+                title=html.escape(title), blurb=blurb, children_links="{children_links}"
             )
-            pages_list.append({"filename": filename, "content": page_html, "uid": uid, "is_branch": True})
-            
+            pages_list.append(
+                {
+                    "filename": filename,
+                    "content": page_html,
+                    "uid": uid,
+                    "is_branch": True,
+                }
+            )
+
         elif m_type == "alias":
             target = meta.get("target_uid")
             if target:
-                self.uid_to_filename[uid] = self.uid_to_filename.get(target, f"{target.replace('/', '_')}.html")
+                self.uid_to_filename[uid] = self.uid_to_filename.get(
+                    target, f"{target.replace('/', '_')}.html"
+                )
             return None
 
         return filename, collected_headers
