c4c45516b70108545a9fe4b4b59a6fc6175b506a
Vijjo <vjjdaya@gmail.com>
2026-05-19 23:48:33 +0545

    fix: close lookup-popup on navigation by dispatching popup:close-all event

--- web/assets/modules/ui/components/popup/index.js
+++ web/assets/modules/ui/components/popup/index.js
@@ -3,7 +3,13 @@ import { PopupOrchestrator } from './popup_orchestrator.js';
 
 export const PopupAPI = {
     scan: () => PopupOrchestrator.scanComments(),
-    hideAll: (skipSnapshot = false) => PopupOrchestrator.closeAll(skipSnapshot),
+    hideAll: (skipSnapshot = false) => {
+        // [MODIFIED] Dispatch event instead of direct call to ensure 
+        // decoupled components (like LookupManager) also respond.
+        window.dispatchEvent(new CustomEvent('popup:close-all', { 
+            detail: { skipSnapshot } 
+        }));
+    },
     restore: () => PopupOrchestrator.restoreState(),
     init: () => PopupOrchestrator.init()
 };
